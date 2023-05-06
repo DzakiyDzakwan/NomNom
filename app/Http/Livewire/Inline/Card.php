cb96fe685a7668cba3dc4e0a718f92b0a2bce504
talitha <talithasyafiyah1112@gmail.com>
2023-05-06 16:03:59 +0700

    component_feature:menambah card component

---
+++ app/Http/Livewire/Inline/Card.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Inline;
+
+use Livewire\Component;
+
+class Card extends Component
+{
+    public function render()
+    {
+        return view('livewire.inline.card');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::get('/', 'App\Http\Livewire\Inline\TestComponent'::class)->name('landing-page');
+
+Route::get('/', 'App\Http\Livewire\Inline\Modal'::class)->name('landing-page');
 Route::get('/test', function(){
     return view('test');
 });
