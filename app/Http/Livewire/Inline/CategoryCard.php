712c4dc330944b6725518950ffba9625ba53844e
talitha <talithasyafiyah1112@gmail.com>
2023-05-07 17:47:51 +0700

    talitha_component:menambahkan category image card component

---
+++ app/Http/Livewire/Inline/CategoryCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Inline;
+
+use Livewire\Component;
+
+class CategoryCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.inline.category-card');
+    }
+}
---
+++ resources/views/livewire/inline/category-card.blade.php
@@ -0,0 +1,73 @@
+<section class="container px-10 mx-auto text-gray-600 body-font mt-12">
+  <div class="">
+    <div class="flex flex-col text-center w-full mb-6">
+      <h1 class="sm:text-3xl text-2xl font-bold title-font text-gray-900">Kategori</h1>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/sarapan.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Sarapan</h1>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/lunch.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Makan Siang</h1>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/dinner.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Makan Malam</h1>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/ramadhan.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Ramadhan</h1>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/kue.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Kue</h1>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/appetizer.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Hidangan Pembuka</h1>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/dessert.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Hidangan Penutup</h1>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/4 md:w-1/4 sm:w-1/2 p-4 bg-base-100 px-2 py-2">
+        <div class="flex relative">
+          <img alt="gallery" class="absolute rounded-2xl h-full w-full object-cover" src="{{ asset('assets/images/minuman.jpg') }}">
+          <div class="rounded-2xl px-8 bottom-0 left-0 right-0 py-24 pb-20 relative z-10 w-full bg-gray-800 opacity-60">
+            <h1 class="text-xl font-semibold text-white pb-1 text-center shadow-lg">Minuman</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
