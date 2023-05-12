dcb2fe5dbc6e83aef9f5ef92c7823d331060dd13
HelloitsMutiaa <rahmahm868@gmail.com>
2023-05-13 02:34:14 +0700

    adding biodata-section

---
+++ resources/views/livewire/biodata-section.blade.php
@@ -0,0 +1,43 @@
+<div>
+    <section class="bg-[#383838] text-white h-[323px]">
+        <div class="max-w-screen-xl px-4 py-10">
+          <div class="max-w-xl ml-32 relative">
+            <div class="md:w-[179px] md:h-[179px]">
+                <div class="justify-center">
+                    <img src="{{ asset('assets/images/profile.jpg') }}" class="w-[100%] h-[100%] rounded-full relative" alt="" srcset="">                     
+                </div>
+                <button class="h-10 w-10 fixed bg-white ml-36 top-40 rounded-full border-solid border-2 border-[#FFB03E] px-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-[#FFB03E]">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                      </svg>                                           
+                </button>
+            </div>
+            <div class="w-[879px] h-60 ml-56 absolute top-8">
+                <div class="text-white text-4xl font-thin mb-5">Demian Schnaffhold</div>
+                <div class="text-white text-lg font-light">Seorang Mahasiswa di MIT</div>
+                <ul class="flex mb-10">
+                    <li class="text-white text-base font-bold mr-1">18</li>
+                    <li class="text-white text-base font-extralight mr-3">Total Recipes</li>
+                    <li class="h-6 w-6 rounded-full mr-1 justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                          </svg>                          
+                    </li>
+                    <li class="text-white text-base font-extralight mr-3">1.220 Followers</li>
+                    <li class="h-6 w-6 rounded-full justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                          </svg>                                                  
+                    </li>
+                    <li class="text-white text-base font-extralight">Medan</li>
+                </ul>
+                <div class="text-white text-sm font-bold">Tentang Saya</div>
+                <div class="text-white text-base font-extralight">
+                    "Masakan adalah seni, dan saya senang berbagi kreativitas dan kecintaan saya pada masakan dengan dunia. Saya akan membagikan resep, ide, dan tips untuk memperluas keterampilan memasak Anda." 
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+</div>
