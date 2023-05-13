094d15e73e48ddd312f5c025aa3a359f6e23301a
talitha <talithasyafiyah1112@gmail.com>
2023-05-14 00:23:24 +0700

    talitha_component: Menambahkan filter button component dan memperbaiki hero section

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -20,8 +20,15 @@
         </div>
     @elseif($type = "regular" && $withGradient)
         <button {{ $attributes->merge(['class' => "flex bg-gradient-to-r from-[#FFB03E] to-[#F67356] text-white duration-500 px-6 py-2 hover:bg-cyan-500 rounded"]) }} >
-            {{ $icon }}
+            {{-- {{ $icon }} --}}
             <span class="basis-full pr-4 text-main">{{ $text }}</span>
+        </button>
+    @elseif($type = "filter" && !$withGradient)
+        <button {{ $attributes->merge(['class' => "flex outline outline-[#FFB03E] text-gray-800 duration-500 px-4 py-1 hover:bg-[#FFB03E] hover:text-white rounded-full"]) }} >
+            <span class="basis-full pr-1 text-base font-medium">{{ $text }}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
         </button>
     @endif
 </div>
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,12 +1,36 @@
 <div>
-    <div class="bg-[url('../../public/assets/images/bg-hero.jpg')] bg-no-repeat bg-top">
+    <div class="bg-[url('../../public/assets/images/bg-hero.jpg')] bg-no-repeat bg-contain">
         @livewire('navbar', ['counter' => $counter])
-        @livewire('inline.hero-section')
+        
+        <section class="text-gray-600 mt-[52px] mb-12">
+            <div class="container mx-auto flex px-10 pt-8 md:flex-row flex-col items-center relative">
+                <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-[5rem] items-center text-center">
+                    <h1 class="font-bold sm:text-5xl text-5xl mb-8 text-gray-900">Masak Apa Hari Ini?</h1>
+                    <p class="mb-6 leading-relaxed text-base font-medium max-w-3xl">Temukan resep yang kamu inginkan berdasarkan kategori yang disediakan atau bahan yang kamu miliki serta buat resep versimu sendiri dan posting agar dapat dilihat oleh banyak orang! <br> Semua itu dapat kamu lakukan di NomNom.</p>
+                    <form class="flex items-center">
+                        <label for="simple-search" class="sr-only">Search</label>
+                        <div class="relative">
+                            <input type="text" id="simple-search" class="w-full bg-transparent backdrop-blur-sm text-[#616161] border border-[#838383] shadow-sm text-gray-800 text-base font-medium focus:ring-blue-500 focus:border-blue-500 block w-[300px] h-[40px] md:w-[360px] pl-10 p-2.5 rounded-lg" placeholder="Cari Resep..." required>
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" class="w-5 h-5 text-[#3E3E3E] dark:text-gray-400" fill="#3E3E3E" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                        </div>
+                    </form>
+                    {{-- <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                    <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button> --}}
+                </div>
+                <div class="w-[553px] h-[550px] w-1/2 w-5/6">
+                    <img class="object-cover object-center drop-shadow-xl" alt="hero" src="{{ asset('assets/images/hero.png') }}">
+                </div>
+            </div>
+        </section>
+        
+        <section class="container px-10 mx-auto text-gray-600 body-font mt-12">
+            @livewire('inline.category-card')
+        </section>
     </div>
-
-    <section class="container px-10 mx-auto text-gray-600 body-font mt-12">
-        @livewire('inline.category-card')
-    </section>
 
     <div class="text-center mt-9">
         <x-button type="rounded" :with-gradient=true text="Lihat Lainnya"></x-button>
@@ -17,8 +41,13 @@
     </div>
 
     <section class="container px-10 mt-6 mx-auto">
+        <div class="mb-8">
+            @livewire('inline.filter-button')
+        </div>
+        
         <div class="flex flex-wrap -m-4">
-            @for ($i = 0; $i < 8; $i++) @livewire('inline.card') @endfor </div>
+            @for ($i = 0; $i < 8; $i++) @livewire('inline.card') @endfor
+        </div>
     </section>
 
     @livewire('pagination')
