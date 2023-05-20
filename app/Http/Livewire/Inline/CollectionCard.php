63f25c60b1df59c81c8d584e82a60b90dc69fbcf
HelloitsMutiaa <rahmahm868@gmail.com>
2023-05-20 23:22:54 +0700

    adding saved-collection page

---
+++ app/Http/Livewire/Inline/CollectionCard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Inline;
+
+use Livewire\Component;
+
+class CollectionCard extends Component
+{
+    public function render()
+    {
+        return view('livewire.inline.collection-card');
+    }
+}
--- resources/views/livewire/saved-collections.blade.php
+++ resources/views/livewire/saved-collections.blade.php
@@ -7,8 +7,8 @@
 
     <div class="px-32 relative flex py-10">
         <div class="flex">
-            <x-button type="regular" :with-gradient=false text="List Resep"></x-button>
-            <a href="saved-collections" class="ml-10 text-base font-semibold my-auto">Koleksi Tersimpan</a>
+            <a href="profile" class="text-base font-semibold my-auto mr-10">List Resep</a>
+            <x-button type="regular" :with-gradient=false text="Koleksi Tersimpan"></x-button>
         </div>
         <div class="absolute right-32 flex my-auto">
             <p class="text-lg mr-4 font-normal">Tampilkan: </p>
@@ -25,12 +25,19 @@
         </div>
     </div>
 
-    <section class="container px-10 mx-auto mb-14">
-        <div class="flex flex-wrap -m-4">
-            @for ($i = 0; $i < 8; $i++) @livewire('inline.card') @endfor
+    <section class="container mx-auto mb-48 px-32">
+        <div class="flex flex-wrap -m-4 mb-20">
+            @for ($i = 0; $i < 4; $i++) @livewire('inline.collection-card') @endfor
+        </div>
+        <div class="absolute right-32 flex my-auto">
+            <button class="items-center rounded-full bg-white p-6 outline outline-gray-200 shadow-2xl">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8">
+                    <path fill="#FFB03E" d="M21 7h-2v2h-2V7h-2V5h2V3h2v2h2v2zm-2 14l-7-3l-7 3V5c0-1.1.9-2 2-2h7a5.002 5.002 0 0 0 5 7.9V21z"/>
+                </svg>
+            </button>
         </div>
     </section>
-
+</div>
     <x-footer></x-footer>
     
 </div>
