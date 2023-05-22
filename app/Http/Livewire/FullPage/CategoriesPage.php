d788557b873a875a319b1237df5aa41cbecaf341
Dzakiy Dzakwan <dzakcart@gmail.com>
2023-05-22 23:08:09 +0700

    Pagination

--- app/Http/Livewire/FullPage/CategoriesPage.php
+++ app/Http/Livewire/FullPage/CategoriesPage.php
@@ -4,9 +4,13 @@ namespace App\Http\Livewire\FullPage;
 
 use Livewire\Component;
 use App\Models\Resep;
+use Livewire\WithPagination;
 
 class CategoriesPage extends Component
 {
+
+    use WithPagination;
+
     public $filter, $kategori, $subkategori;
 
     protected $listeners = ['changeFilter'];
@@ -15,12 +19,12 @@ class CategoriesPage extends Component
     {
         if($this->filter) {
             if ($this->filter == 2) {
-                $resep = Resep::latest()->paginate(3);
+                $resep = Resep::latest()->paginate(2);
             } else {
-                $resep = Resep::oldest()->paginate(3);
+                $resep = Resep::oldest()->paginate(2);
             }
         } else {
-            $resep = Resep::paginate(3);
+            $resep = Resep::paginate(2);
         }
         
         return view('livewire.categories-page', compact('resep'))->layout('layouts.main', ['title' => 'Kategori']);
